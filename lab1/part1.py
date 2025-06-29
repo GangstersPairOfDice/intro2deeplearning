2afe3496c69e48dd928fc131ebc4aad78c6d2f56
GangsterPairOfDice <daniel.lyakhovich@gmail.com>
2025-06-29 20:34:47 +0200

    removed bloat

--- lab1/part1.py
+++
@@ -1,8 +0,0 @@
-# imports
-import mitdeeplearning as mdl
-from tinygrad import Device
-import numpy as np
-import matplotlib.pyplot as plt
-
-
-print(Device.DEFAULT) # prints default device
--- lecture1/test.py
+++
@@ -1,5 +0,0 @@
-from tinygrad import Tensor, nn
-
-test = Tensor([1,2,3,4]).grad().numpy()
-
-print(test)
